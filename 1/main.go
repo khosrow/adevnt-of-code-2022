5905eb56b6993876b9df7d9b18b91355622affdd
Khosrow Ebrahimpour <khosrow.ebrahimpour@shopify.com>
2022-12-01 11:39:29 -0500

    day 1

---
+++ 1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"sort"
+)
+
+func main() {
+
+	input, err := os.ReadFile("input1.txt")
+	// input, err := os.ReadFile("example.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Part 1
+	calories := strings.Split(string(input), "\n")
+	var elves []int
+
+	elf := 0
+	for _, calorie := range calories {
+		if calorie == "" {
+			elves = append(elves, elf)
+			elf = 0
+		} else {
+			c, _ := strconv.Atoi(calorie)
+			elf += c
+		}
+	}
+
+	sort.Sort(sort.IntSlice(elves))
+	fmt.Println("Highest:", elves[len(elves)-1])
+	fmt.Println("Top Three:", elves[len(elves)-1], elves[len(elves)-2], elves[len(elves)-3])
+	fmt.Println("Top Three Total:", elves[len(elves)-1]+elves[len(elves)-2]+elves[len(elves)-3])
+}
+
+
